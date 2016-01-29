7d1bf80f7f0c403d5f4df3051606a8b939c3b8f5
drewmoto <drewmoto@gmail.com>
2016-01-29 15:40:29 -0800

    Classwork Updates for Weeks 5 and 6

---
+++ ClassWork-06/Classwork 2 JavaScript Week 6/javascript.js
@@ -0,0 +1,16 @@
+function calcCelciusToFahrenheit (num) {
+return (num * (9/5) + 32)
+}
+
+function calcFahrenheitToCelcius(num) {
+ return (num - 32) * 5/9
+}
+
+function calcCircumference (num) {
+  return (2 * Math.PI * num)
+}
+
+function calcLongestSide (a, b) {
+  return (Math.sqrt((a * a) + (b * b)))
+
+}
